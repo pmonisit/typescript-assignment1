56cd4f341c1bfef7cdbd0faeefcd88e3c6e9a6d9
Prince Andwill <pmonisit.a6162831@gmail.com>
2022-08-09 09:09:56 +0800

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 // 1. City Directory
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.assignment4 = exports.assignment3 = exports.checkIsbn = exports.clearStorage = exports.displayData = exports.addData = void 0;
+exports.assignment4 = exports.assignment3 = exports.checkIsbn = exports.displayData = exports.addData = void 0;
 var cityList = [];
 const addData = (data) => {
     cityList.push(data);
@@ -25,79 +25,22 @@ const displayData = () => {
     });
 };
 exports.displayData = displayData;
-const clearStorage = () => localStorage.clear();
-exports.clearStorage = clearStorage;
-var render = (data) => {
-    var app = document.getElementById('list-view');
-    var cityHTMLString = '<ul>' +
-        data.map((cityDirectory) => {
-            return '<li>' +
-                '<strong>Title: </strong>' + cityDirectory.city + '<br/>' +
-                '<strong>Subtitle: </strong>' + cityDirectory.country + '<br/>' +
-                '<strong>Author: </strong>' + cityDirectory.population + '<br/>' +
-                '</li>';
-        }).join('');
-    +'</ul>';
-    app.innerHTML = cityHTMLString;
-};
-render(cityList);
-var handleSearch = (event) => {
-    event.preventDefault();
-    // Get the search terms from the input field
-    var searchTerm = event.target.elements['search'].value;
-    // Tokenize the search terms and remove empty spaces
-    var tokens = searchTerm
-        .toLowerCase()
-        .split(' ')
-        .filter((token) => {
-        return token.trim() !== '';
-    });
-    if (tokens.length) {
-        //  Create a regular expression of all the search terms
-        var searchTermRegex = new RegExp(tokens.join('|'), 'gim');
-        var filteredList = cityList.filter((cityDirectory) => {
-            // Create a string of all object values
-            var cityString = '';
-            for (var key in cityDirectory) {
-                if (cityDirectory.hasOwnProperty(key) && cityDirectory[key] !== '') {
-                    cityString += cityDirectory[key].toString().toLowerCase().trim() + ' ';
-                }
-            }
-            // Return book objects where a match with the search regex if found
-            return cityString.match(searchTermRegex);
-        });
-        // Render the search results
-        render(filteredList);
-    }
-};
-document.addEventListener('submit', handleSearch);
-document.addEventListener('reset', function (event) {
-    event.preventDefault();
-    render(cityList);
-});
 // 2. ISBN – 10 Validation
 const checkIsbn = (isbn) => {
     if (isbn.length != 10)
         return console.log(false);
-    // Computing weighted sum of
-    // first 9 digits
-    let sum = 0;
+    let totalSum = 0;
     for (let i = 0; i < 9; i++) {
         let digit = isbn[i] - 0;
         if (0 > digit || 9 < digit)
             return console.log(false);
-        sum += (digit * (10 - i));
+        totalSum += (digit * (10 - i));
     }
-    // Checking last digit.
-    let last = isbn[9];
-    if (last != 'X' && (last < '0' || last > '9'))
+    let lastDigit = isbn[9];
+    if (lastDigit != 'X' && (lastDigit < '0' || lastDigit > '9'))
         return console.log(false);
-    // If last digit is 'X', add 10
-    // to sum, else add its value.
-    sum += ((last == 'X') ? 10 : (last - 0));
-    // Return true if weighted sum
-    // of digits is divisible by 11.
-    if (sum % 11 == 0) {
+    totalSum += ((lastDigit == 'X') ? 10 : (lastDigit - 0));
+    if (totalSum % 11 == 0) {
         return console.log(true);
     }
     else {
@@ -105,26 +48,21 @@ const checkIsbn = (isbn) => {
     }
 };
 exports.checkIsbn = checkIsbn;
+(0, exports.checkIsbn)("1112223339"); //true
 (0, exports.checkIsbn)("111222333"); //false
 // 3. Change it up!
-const assignment3 = (str) => {
-    var result = "";
-    for (var i = 0; i < str.length; i++) {
-        if (122 == str.charCodeAt(i)) {
-            result += "a";
-        }
-        else if (90 == str.charCodeAt(i)) {
-            result += "A";
-        }
-        else if ((65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 89) ||
-            (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 121)) {
-            result += String.fromCharCode(str.charCodeAt(i) + 1);
+const assignment3 = (inputString) => {
+    var modifiedString = "";
+    for (var i = 0; i < inputString.length; i++) {
+        if ((65 <= inputString.charCodeAt(i) && inputString.charCodeAt(i) <= 90) ||
+            (97 <= inputString.charCodeAt(i) && inputString.charCodeAt(i) <= 122)) {
+            modifiedString += String.fromCharCode(inputString.charCodeAt(i) + 1);
         }
         else {
-            result += str.charAt(i);
+            modifiedString += inputString.charAt(i);
         }
     }
-    return result.split('').reduce((a, c) => a + (/[aeiou]/i.test(c) ? c.toUpperCase() : c.toLowerCase()), "");
+    return modifiedString.split('').reduce((curr, next) => curr + (/[aeiou]/i.test(next) ? next.toUpperCase() : next.toLowerCase()), "");
 };
 exports.assignment3 = assignment3;
 console.log((0, exports.assignment3)("Cat30")); // Output is dbU30
--- index.ts
+++ index.ts
@@ -25,118 +25,52 @@ export const displayData = () => {
     })
 }
 
-export const clearStorage = () => localStorage.clear()
-
-var render = (data: any) => {
-    var app = document.getElementById('list-view')!;
-    var cityHTMLString = '<ul>'+
-    data.map( (cityDirectory: any) => {
-        return '<li>'+
-                '<strong>Title: </strong>' + cityDirectory.city + '<br/>' +
-                '<strong>Subtitle: </strong>' + cityDirectory.country + '<br/>' +
-                '<strong>Author: </strong>' + cityDirectory.population + '<br/>' +
-              '</li>';
-      }).join('');
-      + '</ul>';
-
-    app.innerHTML = cityHTMLString;
-  }
-  render(cityList);
-
-  var handleSearch = (event: any) => {
-    event.preventDefault();
-    // Get the search terms from the input field
-    var searchTerm = event.target.elements['search'].value;
-    // Tokenize the search terms and remove empty spaces
-    var tokens = searchTerm
-                  .toLowerCase()
-                  .split(' ')
-                  .filter((token: any) => {
-                    return token.trim() !== '';
-                  });
-   if(tokens.length) {
-    //  Create a regular expression of all the search terms
-    var searchTermRegex = new RegExp(tokens.join('|'), 'gim');
-    var filteredList = cityList.filter( (cityDirectory) => {
-      // Create a string of all object values
-      var cityString = '';
-      for(var key in cityDirectory) {
-        if(cityDirectory.hasOwnProperty(key) && cityDirectory[key] !== '') {
-          cityString += cityDirectory[key].toString().toLowerCase().trim() + ' ';
-        }
-      }
-      // Return book objects where a match with the search regex if found
-      return cityString.match(searchTermRegex);
-    });
-    // Render the search results
-    render(filteredList);
-   }
-  };
-
-  document.addEventListener('submit', handleSearch);
-  document.addEventListener('reset', function(event){
-    event.preventDefault();
-    render(cityList);
-  })
-
-
 
 // 2. ISBN – 10 Validation
-export const checkIsbn = (isbn: string | any ) => {
+export const checkIsbn = (isbn: any) => {
         if (isbn.length != 10)
             return console.log(false);
-        // Computing weighted sum of
-        // first 9 digits
-        let sum = 0;
+       
+        let totalSum = 0;
         for (let i = 0; i < 9; i++)
         {
-            let digit = isbn[i] - 0;
-                
+            let digit = isbn[i] - 0;      
             if (0 > digit || 9 < digit)
-                return console.log(false);
-                    
-            sum += (digit * (10 - i));
+                return console.log(false);           
+                totalSum += (digit * (10 - i));
         }
-        // Checking last digit.
-        let last = isbn[9];
-        if (last != 'X' && (last < '0' || last > '9'))
+        
+        let lastDigit = isbn[9];
+        if (lastDigit != 'X' && (lastDigit < '0' || lastDigit > '9'))
             return console.log(false);
+            totalSum += ((lastDigit == 'X') ? 10 : (lastDigit - 0));
    
-        // If last digit is 'X', add 10
-        // to sum, else add its value.
-        sum += ((last == 'X') ? 10 : (last - 0));
-   
-        // Return true if weighted sum
-        // of digits is divisible by 11.
-        if(sum % 11 == 0){
+        if(totalSum % 11 == 0){
             return console.log(true);
         }else{
             return console.log(false);
-        }
-        
+        }      
 }
+checkIsbn("1112223339"); //true
 checkIsbn("111222333"); //false
 
 
 
 // 3. Change it up!
-export const assignment3 = (str: string) => {
-    var result = "";
-    for (var i = 0; i < str.length; i++) {    
-        if (122 == str.charCodeAt(i)) {
-            result += "a";      
-        } else if (90 == str.charCodeAt(i)) {
-            result += "A"; 
-        } else if ((65 <= str.charCodeAt(i) && str.charCodeAt(i) <= 89) ||
-                   (97 <= str.charCodeAt(i) && str.charCodeAt(i) <= 121)) {
-            result += String.fromCharCode(str.charCodeAt(i) + 1);
+export const assignment3 = (inputString: string) => {
+    var modifiedString = "";
+    for (var i = 0; i < inputString.length; i++) {    
+         if ((65 <= inputString.charCodeAt(i) && inputString.charCodeAt(i) <= 90) ||
+             (97 <= inputString.charCodeAt(i) && inputString.charCodeAt(i) <= 122)) {
+                    modifiedString += String.fromCharCode(inputString.charCodeAt(i) + 1);
         } else {
-            result += str.charAt(i);
+            modifiedString += inputString.charAt(i);
         }
     }
-    return result.split('').reduce((a, c) => a + (/[aeiou]/i.test(c) ? c.toUpperCase() : c.toLowerCase()), "");
+    return modifiedString.split('').reduce((curr, next) => curr + (/[aeiou]/i.test(next) ? next.toUpperCase() : next.toLowerCase()), "");
 }
 console.log(assignment3("Cat30")); // Output is dbU30
+
 
 
 // 4. Moving Zeroes to the End
